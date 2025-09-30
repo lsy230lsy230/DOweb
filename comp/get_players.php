4b6014c4369223323f86df3e8cdf1364907a4cd5
lsy230lsy230 <lsy230lsy230@gmail.com>
2025-10-01 01:37:12 +0900

    Add CORS headers to get_players.php - get_players.php??CORS ?ㅻ뜑 異붽?

--- comp/get_players.php
+++ comp/get_players.php
@@ -1,5 +1,16 @@
 <?php
-header('Content-Type: application/json');
+// CORS 헤더 추가
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json; charset=utf-8');
+
+// OPTIONS 요청 처리 (preflight)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
